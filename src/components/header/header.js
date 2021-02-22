88f5713ea08d8f58fe36e78640e4811950183426
Mauricio Cordero <mrc.cordero@gmail.com>
2021-02-22 00:38:51 -0300

    adding logo, styles, and icons fixes sizes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import './header.css'
-import Brightness2OutlinedIcon from '@material-ui/icons/Brightness2Outlined';
-import LanguageOutlinedIcon from '@material-ui/icons/LanguageOutlined';
-import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined';
+import Brightness2OutlinedIcon from '@material-ui/icons/Brightness2Outlined'
+import LanguageOutlinedIcon from '@material-ui/icons/LanguageOutlined'
+import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined'
+import Logo from './../../assets/images/logo.png'
 
 const Header = () => (
     <header className="header">
-        <div className="icon-btn">
-            <LanguageOutlinedIcon />
+        <div className="logo">
+            <img src={Logo} alt="Macord developer"/>
         </div>
-        <div className="icon-btn">
-            <Brightness2OutlinedIcon />
-        </div>
-        <div className="icon-btn">
-            <MenuOutlinedIcon />
+        <div className="actions">
+            <div className="icon-btn">
+                <LanguageOutlinedIcon />
+            </div>
+            <div className="icon-btn">
+                <Brightness2OutlinedIcon />
+            </div>
+            <div className="icon-btn">
+                <MenuOutlinedIcon />
+            </div>
         </div>
     </header>
 )
--- src/components/nav-menu/nav-menu.js
+++ src/components/nav-menu/nav-menu.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import './nav-menu.css'
+import PermIdentityOutlinedIcon from '@material-ui/icons/PermIdentityOutlined'
+import CodeOutlinedIcon from '@material-ui/icons/CodeOutlined'
+import DeveloperBoardOutlinedIcon from '@material-ui/icons/DeveloperBoardOutlined';
+import AccountBoxOutlinedIcon from '@material-ui/icons/AccountBoxOutlined'
 
 const NavMenu = () => (
     <nav className="nav-menu">
-        <h1>NavMenu</h1>
+        <div className="icon-btn">
+            <PermIdentityOutlinedIcon />
+        </div>
+        <div className="icon-btn">
+            <CodeOutlinedIcon />
+        </div>
+        <div className="icon-btn">
+            <DeveloperBoardOutlinedIcon />
+        </div>
+        <div className="icon-btn">
+            <AccountBoxOutlinedIcon />
+        </div>
     </nav>
 )
 
